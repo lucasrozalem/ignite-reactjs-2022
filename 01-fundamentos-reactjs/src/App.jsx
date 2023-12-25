664a5815129bfb851ecf4852844f17521fe908e0
Lucas <lrozalem@gmail.com>
2023-12-25 20:07:31 -0300

    feat: css modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,10 @@
 import { Post } from "./Post";
-
+import { Header } from "./components/Header";
+import "./styles.css";
 function App() {
   return (
     <div>
+      <Header/>
       <Post 
       author="Lucas"
       content="Post"
